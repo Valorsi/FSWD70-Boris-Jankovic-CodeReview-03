8fa3dc4cba5aeb9e002a9c5a3a4ff1bd4685e98d
Boris Jankovic <borisjanko@outlook.com>
2019-05-25 12:15:25 +0200

    add scripts

---
+++ Javascript/script.js
@@ -0,0 +1,26 @@
+function calculateInsurance(){ 
+	var c = document.getElementById('form3');
+	var country = c.elements["country"].value
+	var a = document.getElementById('form2');
+	var age = parseInt(a.elements["age"].value);
+	var h = document.getElementById('form4');
+	var horse = parseInt(h.elements["horse"].value);
+	var x = document.getElementById('form1');
+	var name = x.elements["name"].value
+if (country == "Austria"){
+ 	var insurance = Math.floor((horse * 100) / age + 50 ) ;
+} 
+else if (country == "Greece"){
+	var insurance = Math.floor((horse *150) / ( age + 2 ) + 50 );
+	
+}
+else if (country == "Hungary"){
+	var insurance = Math.floor((horse *120) / age + 100);
+
+}
+else {
+	var insurance = " Please pick a Country "
+}
+document.getElementById("result").innerHTML = name + " ,your insurance costs " + insurance + " €"
+document.getElementById("result").style.display = "block";
+}
